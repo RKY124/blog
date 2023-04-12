82dfc6ff3a0c6257c4615d0f071d85d0149484c4
kh7j <kh7j@fresnou.org>
2023-04-12 11:08:42 -0700

    commit wednesday

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,5 @@ router.post('/article/:articleId/edit', articleController.updateArticle);
 router.get('/article/:articleId/delete', articleController.deleteArticle);
 
 router.post('/article/:articleId/comment/create', commentController.createComment);
+router.post('/comment/:commentId/reply/create', commentController.addReply);
 module.exports = router;
